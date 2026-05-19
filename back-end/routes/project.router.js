b22367cb17dcb3b927635cbe9fdf8c2b3eb176ba
Ayoub Jabiri <ayoubjabiri.business@gmail.com>
2026-05-19 10:47:37 +0100

    Set up the back-end

---
+++ back-end/routes/project.router.js
@@ -0,0 +1,55 @@
+// External Modules
+import { Router } from "express";
+
+// Internal Modules
+import {
+    getProjects,
+    register,
+    update,
+    deletePr,
+} from "../controllers/project.controller.js";
+import {
+    projectRules,
+    updateRules,
+    projectValidation,
+    projectCheck,
+    verifyOwnership,
+} from "../middlewares/project.middleware.js";
+import {
+    authenticationCheck,
+    authorizationCheck,
+} from "../middlewares/auth.middleware.js";
+
+const projectRoutes = Router();
+
+projectRoutes.use(authenticationCheck);
+
+projectRoutes.get("/", authorizationCheck(["owner"]), getProjects);
+
+projectRoutes.post(
+    "/register",
+    authorizationCheck(["owner"]),
+    projectRules,
+    projectValidation,
+    register
+);
+
+projectRoutes.put(
+    "/:id",
+    authorizationCheck(["owner"]),
+    projectCheck,
+    verifyOwnership,
+    updateRules,
+    projectValidation,
+    update
+);
+
+projectRoutes.delete(
+    "/:id",
+    authorizationCheck(["owner"]),
+    projectCheck,
+    verifyOwnership,
+    deletePr
+);
+
+export default projectRoutes;
---
+++ back-end/services/project.service.js
@@ -0,0 +1,25 @@
+import Project from "../models/project.schema.js";
+
+export const registerProject = async (project) => await Project.create(project);
+
+export const getProject = async (query) => await Project.findOne(query);
+
+export const getOwnerProjects = async (ownerId) =>
+    await Project.find({ ownerId });
+
+export const deleteProject = async (projectId) =>
+    await Project.deleteOne(projectId);
+
+export const updateProject = async (id, newData) => {
+    const project = await Project.findById(id);
+
+    project.title = newData.title;
+    project.description = newData.description;
+    project.capital = newData.capital;
+    project.maxPercentage = newData.maxPercentage;
+
+    if (project.capital > project.currentAmount)
+        project.status = newData.status;
+
+    return await project.save();
+};
