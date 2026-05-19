135dda1c639d21239e2ad3aa89c1146809ff57fd
Ayoub Jabiri <ayoubjabiri.business@gmail.com>
2026-05-19 15:51:01 +0100

    Handle clicking on logout button

--- front-end/src/components/Layout/SideBar.jsx
+++ front-end/src/components/Layout/SideBar.jsx
@@ -2,6 +2,10 @@ import { RiBriefcaseLine, RiDashboardLine, RiUserFill } from "@remixicon/react";
 import { Link } from "react-router";
 
 export default function SideBar() {
+    function handleLogout() {
+        localStorage.removeItem("accessToken");
+    }
+
     return (
         <aside className="relative bg-[#13151c] w-[250px] h-screen p-4 border-r border-[#252839]">
             <div className="flex items-center gap-2 p-6 border-b border-[#E2DDD8]">
@@ -14,14 +18,14 @@ export default function SideBar() {
             </div>
             <div className="p-2 mt-4">
                 <Link
-                    to="/invoices"
+                    to="/dashboard"
                     className="text-white hover:bg-[#F0EDE8] hover:text-black w-full text-start px-4 py-3 mb-2 rounded-md flex items-center gap-1 cursor-pointer main-transition"
                 >
                     <RiDashboardLine className="w-[20px]" />
                     <span>Dashboard</span>
                 </Link>
                 <Link
-                    to="/invoices"
+                    to="/projects"
                     className="text-white hover:bg-[#F0EDE8] hover:text-black w-full text-start px-4 py-3 rounded-md flex items-center gap-1 cursor-pointer main-transition"
                 >
                     <RiBriefcaseLine className="w-[20px]" />
@@ -29,7 +33,7 @@ export default function SideBar() {
                 </Link>
             </div>
             <div className="p-6 border-t border-[#E2DDD8] absolute left-[50%] translate-x-[-50%] bottom-0 w-[calc(100%-16px)]">
-                <Link to="/login">
+                <Link to="/login" onClick={handleLogout}>
                     <div className="flex items-center gap-2 p-2 rounded-md text-white hover:bg-[#F0EDE8] hover:text-black main-transition">
                         <div className="w-[32px] h-[32px] font-bold rounded-[50%] flex justify-center items-center">
                             <RiUserFill className="w-[18px]" />
