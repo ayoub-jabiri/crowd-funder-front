62e516f19cd059e9cebd6df26645fac295e157f1
Ayoub Jabiri <ayoubjabiri.business@gmail.com>
2026-05-21 17:45:20 +0100

    Set up the register page UI

--- front-end/src/routes/router.jsx
+++ front-end/src/routes/router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router";
 import AppLayout from "../components/Layout/AppLayout";
 import DashboradPage from "../pages/DashboradPage";
 import LoginPage from "../pages/LoginPage";
+import RegisterPage from "../pages/RegisterPage";
 
 export const router = createBrowserRouter([
     {
@@ -15,5 +16,9 @@ export const router = createBrowserRouter([
     {
         path: "/login",
         element: <LoginPage />,
+    },
+    {
+        path: "/register",
+        element: <RegisterPage />,
     },
 ]);
