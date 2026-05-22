697f176d05b8c56f2ba276e93005d429e878114d
Ayoub Jabiri <ayoubjabiri.business@gmail.com>
2026-05-22 10:35:35 +0100

    Added 404 not found error page

--- front-end/src/pages/NotFoundPage.jsx
+++ front-end/src/pages/NotFoundPage.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router";
 export default function NotFoundPage() {
     return (
         <div className="text-white flex justify-center items-center flex-col py-10">
-            <h1 className="text-3xl font-bold mb-4">Page Not Found</h1>
+            <h1 className="text-3xl font-bold mb-4">Page Not Found !</h1>
             <Link
                 to="/dashboard"
                 className="bg-white text-black p-2 rounded-md"
