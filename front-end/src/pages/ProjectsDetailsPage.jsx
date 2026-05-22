affcdcdba414872ff62753ed8f7b1d87fa16b675
Ayoub Jabiri <ayoubjabiri.business@gmail.com>
2026-05-22 17:32:27 +0100

    Set up the project info withing the project details page

--- front-end/src/pages/ProjectsDetailsPage.jsx
+++ front-end/src/pages/ProjectsDetailsPage.jsx
@@ -49,9 +49,52 @@ export default function ProjectsDetailsPage() {
                 <RiArrowLeftLongLine />
                 <span>Back to Projects</span>
             </Link>
-            <div>
+            <div className="flex justify-between items-center">
                 <h1 className="text-3xl font-bold">{currentProject.title}</h1>
+                <div className="flex gap-3">
+                    <button className="bg-transparent text-white text-[13px] py-1 px-4 border rounded-md hover:bg-white hover:text-gray-800 cursor-pointer main-transition">
+                        Close
+                    </button>
+                    <button className="bg-transparent text-white text-[13px] py-1 px-4 border rounded-md hover:bg-white hover:text-gray-800 cursor-pointer main-transition">
+                        Edit
+                    </button>
+                    <button className="bg-transparent text-white text-[13px] py-1 px-4 border rounded-md hover:bg-white hover:text-gray-800 cursor-pointer main-transition">
+                        Delete
+                    </button>
+                </div>
             </div>
+            <div className="mt-4">
+                <p className="text-sm text-gray-400 mb-2">
+                    {currentProject.description}
+                </p>
+                <p className="text-sm text-gray-400 mb-2">
+                    Status:{" "}
+                    <span className="text-[#2196F3]">
+                        {currentProject.status}
+                    </span>
+                </p>
+                <p className="text-sm text-gray-400 mb-2">
+                    Capital:{" "}
+                    <span className="text-[#2196F3]">
+                        ${currentProject.capital}
+                    </span>
+                </p>
+                <p className="text-sm text-gray-400 mb-2">
+                    Amount Invested:{" "}
+                    <span className="text-[#2196F3]">
+                        ${currentProject.currentAmount}
+                    </span>
+                </p>
+                <p className="text-sm text-gray-400 mb-2">
+                    Max Percentage Allowed:{" "}
+                    <span className="text-[#2196F3]">
+                        {currentProject.maxPercentage}%
+                    </span>
+                </p>
+            </div>
+            <h2 className="text-base font-bold mt-6 mb-4">
+                Project Investors:
+            </h2>
         </div>
     );
 }
